c3d46646c354382cc67063f7dcded32e50de42c9
Mauricio <stb539@gmail.com>
2024-05-14 21:56:05 -0400

    home page

--- app/src/main/java/mau/example/billeteravirtualkotlin/view/HomePage.kt
+++ app/src/main/java/mau/example/billeteravirtualkotlin/view/HomePage.kt
@@ -1,33 +1,21 @@
 package mau.example.billeteravirtualkotlin.view
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import mau.example.billeteravirtualkotlin.R
 
-class LoginPage : AppCompatActivity() {
+class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login_page)
+        setContentView(R.layout.activity_home_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val btnSendLogin = findViewById<Button>(R.id.btnSendLogin)
-
-        btnSendLogin.setOnClickListener {
-            //val intent = Intent(this, HomePageEmptyCase::class.java)
-            val intent = Intent(this, HomePage::class.java)
-            startActivity(intent)
-        }
     }
-
-
 }
