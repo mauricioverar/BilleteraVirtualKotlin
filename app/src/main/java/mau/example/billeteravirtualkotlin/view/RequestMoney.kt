aa16dfc38dfc8654e689021066a5262bdbe240b0
Mauricio <stb539@gmail.com>
2024-05-16 20:41:10 -0400

    requestMoney

--- app/src/main/java/mau/example/billeteravirtualkotlin/view/RequestMoney.kt
+++ app/src/main/java/mau/example/billeteravirtualkotlin/view/RequestMoney.kt
@@ -1,31 +1,27 @@
 package mau.example.billeteravirtualkotlin.view
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import mau.example.billeteravirtualkotlin.R
 
-class HomePage : AppCompatActivity() {
+class RequestMoney : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home_page)
+        setContentView(R.layout.activity_request_money)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val btnRequestMoney = findViewById<Button>(R.id.btnRequestMoney)
-
-        btnRequestMoney.setOnClickListener {
-            //val intent = Intent(this, HomePageEmptyCase::class.java)
-            val intent = Intent(this, RequestMoney::class.java)
-            startActivity(intent)
+        // imagen flecha izquierda
+        findViewById<ImageView>(R.id.left).setOnClickListener {
+            finish() // cerrar actividad y volver a la anterior
         }
     }
 }
