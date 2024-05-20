70349652f83ff466bd566f893ddcd5075df683b5
Mauricio <stb539@gmail.com>
2024-05-20 19:50:18 -0400

    nav_graph

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,9 @@ android {
 }
 
 dependencies {
-
+    // navComponent
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
