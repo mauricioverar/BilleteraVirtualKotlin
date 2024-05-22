aa5d392bad3cca5ed55adf952c64c7c7bf17e1fd
Mauricio <stb539@gmail.com>
2024-05-22 09:43:06 -0400

    left

--- app/src/main/java/mau/example/billeteravirtualkotlin/view/LoginPageFragment.kt
+++ app/src/main/java/mau/example/billeteravirtualkotlin/view/LoginPageFragment.kt
@@ -12,23 +12,19 @@ import mau.example.billeteravirtualkotlin.databinding.FragmentLoginPageBinding
 
 class LoginPageFragment : Fragment() {
 
-    //1
     private lateinit var navController: NavController
     private var _binding: FragmentLoginPageBinding? = null
     private val binding get() = _binding!!
 
-    //2
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentLoginPageBinding.inflate(inflater, container, false)
-        //navController = findNavController()
         // Inflate the layout for this fragment
+        _binding = FragmentLoginPageBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    //3
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
