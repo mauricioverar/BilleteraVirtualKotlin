235511683b97d50f1c4c7c7fccf7428304c7f137
Mauricio <stb539@gmail.com>
2024-05-22 16:21:20 -0400

    navArgs

--- app/src/main/java/mau/example/billeteravirtualkotlin/view/HomePageFragment.kt
+++ app/src/main/java/mau/example/billeteravirtualkotlin/view/HomePageFragment.kt
@@ -1,5 +1,6 @@
 package mau.example.billeteravirtualkotlin.view
 
+//import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,10 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
 import mau.example.billeteravirtualkotlin.R
 import mau.example.billeteravirtualkotlin.databinding.FragmentHomePageBinding
 
 class HomePageFragment : Fragment() {
+    val args: HomePageFragmentArgs by navArgs()
     private lateinit var navController: NavController
     private var _binding: FragmentHomePageBinding? = null
 
@@ -27,6 +30,13 @@ class HomePageFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
+        val username = args.username
+        //val username = arguments?.getString("username")
+
+
+        binding.tvUsername.text = getString(R.string.hello, username)
+        //binding.tvUsername.text = "Hola, $username"
+        //binding.tvUsername.text = "Hola, ${arguments?.getString("username")}"
 
         _binding?.profilePicture?.setOnClickListener {
             navController.navigate(R.id.action_homePageFragment_to_profilePageFragment)
--- app/src/main/java/mau/example/billeteravirtualkotlin/view/LoginPageFragment.kt
+++ app/src/main/java/mau/example/billeteravirtualkotlin/view/LoginPageFragment.kt
@@ -1,16 +1,18 @@
 package mau.example.billeteravirtualkotlin.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import mau.example.billeteravirtualkotlin.R
 import mau.example.billeteravirtualkotlin.databinding.FragmentLoginPageBinding
 
 class LoginPageFragment : Fragment() {
+    //var SM: SendMessage? = null
 
     private lateinit var navController: NavController
     private var _binding: FragmentLoginPageBinding? = null
@@ -33,8 +35,68 @@ class LoginPageFragment : Fragment() {
             navController.navigate(R.id.action_loginPageFragment_to_signupPageFragment)
         }
 
+        // validar email y contraseña
+        val emailRegex = "^[a-zA-Z0-9.!#\$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$"
+        val passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$"
+
+
         _binding?.btnGoToHome?.setOnClickListener {
-            navController.navigate(R.id.action_loginPageFragment_to_homePageFragment2)
+            if (binding.etEmail.text.toString().isEmpty()) {
+                toastEmailEmpty()
+                return@setOnClickListener
+            }
+            if (binding.etPassword.text.toString().isEmpty()) {
+                toastPasswordEmpty()
+                return@setOnClickListener
+            }
+            if (!binding.etEmail.text.toString().matches(emailRegex.toRegex())) {
+                toastEmailFalse()
+                return@setOnClickListener
+            }
+            if (!binding.etPassword.text.toString().matches(passwordRegex.toRegex())) {
+                toastPasswordFalse()
+                return@setOnClickListener
+            }
+
+            //val intent = Intent(context, HomePageFragment::class.java)
+            //intent.putExtra("username", binding.etEmail.text.toString())
+
+            //SM?.sendData(binding.etEmail.text.toString().trim())
+
+            //navController.navigate(R.id.action_loginPageFragment_to_homePageFragment2)
+            navController.navigate(LoginPageFragmentDirections.actionLoginPageFragmentToHomePageFragment2(username = binding.etEmail.text.toString().trim()))
         }
     }
+
+    /*private fun toast() {
+        Toast.makeText(this, "Ingresa nueva contraseña", Toast.LENGTH_SHORT).show()
+    }*/
+
+    private fun toastEmailEmpty() {
+        binding.etEmail.error = "Campo requerido"
+    }
+
+    private fun toastPasswordEmpty() {
+        Toast.makeText(context, "Contraseña requerida", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun toastEmailFalse() {
+        binding.etEmail.error = "Email incorrecto"
+    }
+
+    private fun toastPasswordFalse() {
+        binding.etPassword.error = "Contraseña debe contener al menos 8 caracteres, una letra y un número"
+        //Toast.makeText(this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show()
+    }
+
+    /*interface SendMessage {
+        fun sendData(message: String?)
+    }
+
+    override fun onAttach(context: android.content.Context) {
+        super.onAttach(context)
+        SM = context as SendMessage
+    }*/
+
+
 }
